853351d23b246444aa2aac9e6ee85d97b0cb6929
udeshsharma <udesh2568@gmail.com>
2021-08-30 23:50:23 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_HOST', 'cluster0.dgkcg.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME', 'my-strapi'),
+        username: env('DATABASE_USERNAME', 'Admin'),
+        password: env('DATABASE_PASSWORD', 'admin'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
@@ -18,3 +18,28 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
